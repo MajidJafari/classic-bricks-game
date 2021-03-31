cdb78343382dd519483260c93cef3677539cc5f0
Majid <majidted@gmail.com>
2021-03-31 23:40:31 +0430

    feat: player

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public string moveLeftKeyboard;
+    public string moveRightKeyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var translationAmount = this.speed * Time.deltaTime;
+        if(Input.GetKey(this.moveLeftKeyboard)) {
+            transform.Translate(-translationAmount, 0, 0);
+        }
+        else if (Input.GetKey(this.moveRightKeyboard)) {
+            transform.Translate(translationAmount, 0, 0);
+        }
+
+        var horizontalOffset = this.speed * Time.deltaTime * Input.GetAxis("Mouse X");
+        transform.Translate(horizontalOffset, 0, 0);
+    }
+}
