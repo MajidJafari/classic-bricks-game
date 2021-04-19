908aea849b7a393516903faae507f35157f6e4e7
Majid <majidted@gmail.com>
2021-04-19 23:17:42 +0430

    feat: game over

--- Assets/Scoring.cs
+++ Assets/Scoring.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Scoring : MonoBehaviour
 {
+    public delegate void OnLifeZero();
+    public static event OnLifeZero onLifeZero;
     public int lives;
     private int score { set; get; } = 0;
     public int scoringAmount;
@@ -29,6 +31,9 @@ public class Scoring : MonoBehaviour
     public void LoseLife()
     {
         this.lives--;
+        if (lives <= 0) {
+            LifeIsZero();
+        }
         UpdateScoreboard();
     }
 
@@ -40,4 +45,10 @@ public class Scoring : MonoBehaviour
 
     private void UpdateScoreboard() =>
         this.scoreboard.SetText("Score: " + score + "\n" + "Lives: " + lives);
+
+    void LifeIsZero() {
+        if (onLifeZero != null) {
+            onLifeZero();
+        }
+    }    
 }
