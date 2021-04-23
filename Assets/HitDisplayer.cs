5d532a211058cb7ebff8991f8597121b36bd6edc
Majid <majidted@gmail.com>
2021-04-23 17:58:09 +0430

    feat: hit streak displayer

---
+++ Assets/HitDisplayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDisplayer : MonoBehaviour, IHitListener, ILoseLifeListener
+{
+    public TMPro.TMP_Text displayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable() {
+        Ball.onHit += OnHit;
+        BallRelaunch.onMiss += OnLoseLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHit(HitTypes type, int streak) {
+        if (type == HitTypes.Brick) {
+            Display(streak);
+            if (!displayer.enabled) {
+                displayer.enabled = true;
+            }
+        }
+        
+    }
+
+    public void OnLoseLife() {
+        displayer.enabled = false;
+    }
+
+    private void Display(int streak) {
+        displayer.SetText(streak.ToString());
+    }
+}
